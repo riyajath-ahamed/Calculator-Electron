aeb3f912e0ec71eaee0fa0238dd44880a6c4b038
Riyajatha <riyajath.ahamed@octopusbi.com>
2024-05-22 22:50:09 +0530

    Init

---
+++ main/main.mjs
@@ -0,0 +1,57 @@
+// const { app, BrowserWindow } = require("electron");
+// const serve = require("electron-serve");
+// const path = require("path");
+
+
+import { app, BrowserWindow } from "electron";
+import serve from "electron-serve";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const appServe = app.isPackaged ? serve({
+    directory: path.join(__dirname, "../out")
+}) : null;
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.mjs"),
+            contextIsolation: true,  // ensure context isolation is enabled
+            enableRemoteModule: false // recommended to disable remote module
+        }
+    });
+
+    if (app.isPackaged) {
+        appServe(win).then(() => {
+            win.loadURL("app://-");
+        });
+    } else {
+        win.loadURL("http://localhost:3000");
+        win.webContents.openDevTools();
+        win.webContents.on("did-fail-load", (e, code, desc) => {
+            win.webContents.reloadIgnoringCache();
+        });
+    }
+};
+
+app.on("ready", () => {
+    createWindow();
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
---
+++ main/preload.mjs
@@ -0,0 +1,11 @@
+// import {contextBridge, ipcRenderer} from "electron"
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+    on: (channel, callback) => {
+        ipcRenderer.on(channel, callback);
+    },
+    send: (channel, args) => {
+        ipcRenderer.send(channel, args);
+    }
+});
